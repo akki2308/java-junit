5497c55a4ce3e306653e871836daf2e4a3b785b6
Akki <chauhanakshit61@gmail.com>
2025-04-02 14:55:24 +0530

    [Akshit] ADD: initial commit

---
+++ src/test/java/com/bridgelabz/junittesting/CalculatorTest.java
@@ -0,0 +1,42 @@
+package com.bridgelabz.junittesting;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.bridgelabz.junit.Calculator;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAddition() {
+        assertEquals(10, calculator.add(6, 4));
+        assertEquals(-2, calculator.add(-1, -1));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(5, calculator.subtract(10, 5));
+        assertEquals(-2, calculator.subtract(3, 5));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(20, calculator.multiply(4, 5));
+        assertEquals(0, calculator.multiply(4, 0));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2, calculator.divide(10, 5));
+        assertEquals(-3, calculator.divide(-9, 3));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(10, 0));
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+}
+
